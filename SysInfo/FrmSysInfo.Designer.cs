ca56e3aabf313fe1025060a5fca897028782bac7
13240137763@163.com <qiyubrother>
2020-04-15 16:47:56 +0800

    添加项目文件。

---
+++ SysInfo/FrmSysInfo.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SysInfo
+{
+    partial class FrmSysInfo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSysInfo));
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtSysInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(713, 435);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtSysInfo
+            // 
+            this.txtSysInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSysInfo.BackColor = System.Drawing.Color.Black;
+            this.txtSysInfo.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txtSysInfo.Location = new System.Drawing.Point(13, 13);
+            this.txtSysInfo.Multiline = true;
+            this.txtSysInfo.Name = "txtSysInfo";
+            this.txtSysInfo.ReadOnly = true;
+            this.txtSysInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSysInfo.Size = new System.Drawing.Size(775, 412);
+            this.txtSysInfo.TabIndex = 0;
+            this.txtSysInfo.Text = "正在读取系统信息...";
+            // 
+            // FrmSysInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.txtSysInfo);
+            this.Controls.Add(this.btnCopy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmSysInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "System Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtSysInfo;
+    }
+}
+
---
+++ SysInfo/FrmSysInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SysInfo
+{
+    public partial class FrmSysInfo : Form
+    {
+        public FrmSysInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Thread(new ThreadStart(() => RunCommand("systeminfo"))).Start();
+        }
+
+        public void RunCommand(string cmd)
+        {
+            var strInput = cmd;
+            var p = new Process();
+            //设置要启动的应用程序
+            p.StartInfo.FileName = "cmd.exe";
+            //是否使用操作系统shell启动
+            p.StartInfo.UseShellExecute = false;
+            // 接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardInput = true;
+            //输出信息
+            p.StartInfo.RedirectStandardOutput = true;
+            // 输出错误
+            p.StartInfo.RedirectStandardError = true;
+            //不显示程序窗口
+            p.StartInfo.CreateNoWindow = true;
+            //启动程序
+            p.Start();
+
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(strInput + "&exit");
+
+            p.StandardInput.AutoFlush = true;
+
+            //获取输出信息
+            string strOuput = p.StandardOutput.ReadToEnd();
+            var __ = strOuput.Split(new [] { Environment.NewLine }, StringSplitOptions.None).ToList();
+            __.RemoveRange(0, 5);
+            var sb = new StringBuilder();
+            foreach(var s in __) { sb.AppendLine(s); }
+            strOuput = sb.ToString();
+            BeginInvoke(new Action(() => txtSysInfo.Text = strOuput));
+            
+            //File.AppendAllText(@"c:\install-log.txt", $"[{DateTime.Now.ToString("yyyyMMdd hh:mm:ss fff")}]" + strOuput);
+
+            //等待程序执行完退出进程
+            p.WaitForExit();
+            p.Close();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtSysInfo.Text, TextDataFormat.Text);
+        }
+    }
+}
